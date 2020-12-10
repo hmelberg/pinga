99d6653012294c6310513bc409c45ca838a206f4
Hans Olav Melberg <hans.melberg@gmail.com>
2020-12-10 10:07:47 +0100

    second built attempt

--- pinga/_nbdev.py
+++ pinga/_nbdev.py
@@ -2,9 +2,18 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"expand_hyphen": "notation.ipynb",
+         "del_dot": "notation.ipynb",
+         "del_zero": "notation.ipynb",
+         "get_unique": "notation.ipynb",
+         "expand_star": "notation.ipynb",
+         "expand_colon": "notation.ipynb",
+         "expand_regex": "notation.ipynb",
+         "expand_code": "notation.ipynb",
+         "get_rows": "notation.ipynb"}
 
-modules = ["None.py"]
+modules = ["charlson.py",
+           "notation.py"]
 
 doc_url = "https://hmelberg.github.io/pinga/"
 
